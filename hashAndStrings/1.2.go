ea1c11cfe1db10b29d1353fd9468d9e215fae444
ppmasa8 <kiyokiyopip56@gmail.com>
2021-10-29 16:43:51 +0900

    Solve 1.2

--- hashAndStrings/1.2.go
+++ hashAndStrings/1.2.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 /*** 方針 ***/
 //　２つの文字列が並び替えかどうか調べるには、要素の個数が一致していれば良い。
 // したがって、文字列１のハッシュテーブルを作り、文字列2を前から順にみてハッシュテーブルに
@@ -10,4 +15,27 @@ func main() {
 	str1 := "ohayou"
 	str2 := "ohatou"
 
+	if len(str1) != len(str2) {
+		fmt.Println("false")
+		os.Exit(1)
+	}
+
+	hash := make(map[string]int)
+	for i := 0; i < len(str1); i++ {
+		hash[string(str1[i])]++
+	}
+
+	for j := 0; j < len(str2); j++ {
+		hash[string(str2[j])]--
+	}
+
+	for _,v := range hash {
+		if v == 0 {
+			fmt.Println("false")
+			os.Exit(1)
+		}
+	}
+	fmt.Println("true")
+	os.Exit(0)
 }
+// 時間計算量O(n)

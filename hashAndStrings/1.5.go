db20a7e28ca8523d8067884f08ba06d33698f984
ppmasa8 <kiyokiyopip56@gmail.com>
2021-10-31 18:23:10 +0900

    Solve 1.5

---
+++ hashAndStrings/1.5.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*** 方針 ***/
+// ①番目のstrをハッシュテーブルに展開し、2番目のstrをそこからマイナスする。
+// valueをかぞえ、１または-1が以下のようでなければfalseを返す
+// 1, -1 → 0, 0
+// 1, -1 → 1, 0
+// 1, -1 → 0, 1
+// 1, -1 → 1, 1
+
+func main() {
+	str1, str2 := []string{"s", "r", "f"}, []string{"s", "r", "f", "t"}
+	hash := make(map[string]int)
+	plus, minus := 0, 0
+
+	for _, v := range str1 {
+		hash[string(v)]++
+	}
+
+	for _, v := range str2 {
+		hash[string(v)]--
+	}
+
+	for _, v := range hash {
+		if plus > 1 || minus > 1 {
+			fmt.Println("false")
+			os.Exit(1)
+		} else if v > 1 || v < -1 {
+			fmt.Println("false")
+			os.Exit(1)
+		}
+
+		if v == -1 {
+			minus++
+		} else if v == 1 {
+			plus++
+		}
+	}
+
+	fmt.Println(hash)
+
+	if plus > 1 || minus > 1 {
+		fmt.Println("false")
+		os.Exit(1)
+	} else {
+		fmt.Println("true")
+		os.Exit(0)
+	}
+
+}

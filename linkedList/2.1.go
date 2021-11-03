45ded05d08402852d2dc77f9150be06b1498cbf2
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-03 14:43:16 +0900

    solve 2.1

--- linkedList/2.1.go
+++ linkedList/2.1.go
@@ -1,4 +1,7 @@
 package main
+/*** 方針 ***/
+// ソートされていない連結リストから、重複する要素を削除するには、要素を配列に追加していき、
+// 要素がかぶった場合には、削除してforを回していく。
 
 import (
 	"fmt"
@@ -31,13 +34,40 @@ func (lst *ListNode) Insert(n int) {
 	cp.next = newNode(n, cp.next)
 }
 
+func contains(s []int, e int) bool {
+	for _, v := range s {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	node := newListNode()
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 3; i++ {
 		n := rand.Intn(100)
 		fmt.Println(n)
 		node.Insert(n)
 	}
+	// 重複させる用
+	fmt.Println(10)
+	fmt.Println(10)
+	node.Insert(10)
+	node.Insert(10)
 
+    arr := []int{}
 
+	cp := newListNode()
+
+	for node.head != nil {
+		if contains(arr, node.head.val) {
+			cp.head.next = node.head.next
+		} else {
+			arr = append(arr, node.head.val)
+			cp = node
+		}
+		node.head = node.head.next
+	}
 }
+// 時間計算量O(n)

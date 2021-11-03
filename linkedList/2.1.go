9490e49a44a67b12304dd0260122126fe2142f71
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-03 14:22:27 +0900

    Add node abstruct

--- linkedList/2.1.go
+++ linkedList/2.1.go
@@ -1,11 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Node struct {
 	val  int
 	next *Node
 }
 
-type LinkedList struct {
+type ListNode struct {
 	head *Node
-	len  int
+}
+
+func newNode(x int, cp *Node) *Node {
+	newcp := new(Node)
+	newcp.val, newcp.next = x, cp
+	return newcp
+}
+
+func newListNode() *ListNode {
+	lst := new(ListNode)
+	lst.head = new(Node)
+	return lst
+}
+
+func (lst *ListNode) Insert(n int) {
+	cp := lst.head
+	cp.next = newNode(n, cp.next)
+}
+
+func main() {
+	node := newListNode()
+	for i := 0; i < 5; i++ {
+		n := rand.Intn(100)
+		fmt.Println(n)
+		node.Insert(n)
+	}
+
+
 }
